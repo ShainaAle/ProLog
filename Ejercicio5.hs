801364d0aa5d233f725ce5da90485e9c9c8601fc
ShainaAle <l21140739@queretaro.tecnm.mx>
2024-03-14 15:20:10 -0600

    Ejercicio 5 terminado

--- Ejercicio5.hs
+++ Ejercicio5.hs
@@ -2,4 +2,7 @@
 
 raicesFormulaCuad :: Float-> Float-> Float-> String
 raicesFormulaCuad a b c 
-                        |
+                    |discriminante == 0 = "Solo hay una solucion"
+                    |discriminante > 0 = "Hay dos soluciones"
+                    |discriminante < 0 = "Hay dos soluciones con numeros imaginarios"
+                    where discriminante =  (b ^^ 2) - (4 * a * c)
